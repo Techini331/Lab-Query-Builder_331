c954a5814ed716ba43dd4f36acc466b622f2ef08
Techi Sriwong <you@example.com>
2024-01-05 06:22:08 +0700

    first commit

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,104 @@
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+use Inertia\Inertia;
+ 
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // ได้ข้อมูลแถวแรก 1 record
+        // $emp = DB::table('employees')->first();
+        // ได้ข้อมูล 50 record
+       
+        $dept =DB::table('departments')->orderBy('dept_name')->get();
+
+        $male = DB::table('employees')
+        ->where('first_name', 'like', 'A%')
+        ->where('gender', 'M')
+        ->take(50)
+        ->get();
+
+      
+        $female = DB::table('employees')
+                ->where('gender', 'F')
+                ->whereRaw('(YEAR(NOW()) - YEAR(birth_date)) > 50')
+                ->take(50)
+                ->get();
+
+
+        
+     // เลือกเฉพาะ column : emp_no, first_name, last_name
+     //  $emp = DB::table('departments')->pluck('dept_no' ,'dept_name', );
+ 
+        // $data = json_decode(json_encode($emp), true);
+        
+        // Log::info(  $data);
+        // return response()->json($emp);
+        // return Response($data);
+  
+        // แสดงผลใน .VUE
+        return Inertia::render('Employee/index', [
+            'dept' => $dept,
+            'male' => $male,
+            'female' =>$female
+        ]);
+ 
+    }
+ 
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+ 
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+ 
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+ 
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+ 
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+ 
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/js/Pages/Employee/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+      <div class="one">
+      <h1>No1</h1>
+      <ul>
+      <li v-for="(dept,index) in dept" key="index">{{dept.dept_name}}</li>
+        
+      </ul>
+    </div>
+    </div>
+
+   <hr>
+   <div class="two">
+      <h1 style="font-size:200%">No2</h1>
+      <ul >
+      <li v-for="(male,index) in male" key="index">no.{{index+1}} {{male.first_name}}{{ male.last_name }}  Gender:{{ male.gender }}</li>
+        </ul>
+   </div>
+   
+   <div class="three">
+    <h1 style="font-size:200%">No3</h1>
+      <ul >
+      <li v-for="(female,index) in female" key="index">no.{{index+1}} {{female.first_name}}{{ female.last_name }}  Gender:{{ female.gender }}</li>
+        </ul>
+   </div> 
+
+
+
+</template>
+ 
+<script setup>
+defineProps(["dept","male","female"]);
+</script>
+<style>
+body{
+    text-align: center;
+}
+</style>
